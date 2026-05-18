d40d6d2682f3a7f41bdae2e8301ad12ed2e7a5bb
José Marinho <zemiguelmarinho05@hotmail.com>
2026-05-18 16:30:26 +0100

    Add files via upload

---
+++ Auto_Setup/Tux4_init.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+systemctl restart networking
+
+ifconfig if_e1 172.16.120.254/24
+
+ifconfig if_e2 172.16.121.253/24
+
+sysctl net.ipv4.ip_forward=1
+ 
+sysctl net.ipv4.icmp_echo_ignore_broadcasts=0
+
+iptables -P FORWARD ACCEPT
+
+iptables -F FORWARD
+
+route add -net 172.16.1.0/24 gw 172.16.121.254
