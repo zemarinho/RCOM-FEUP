52c8e9876b8eeb710b8e865717a33075815fedac
Ze <zemiguelmarinho05@hotmail.com>
2026-06-01 14:55:22 +0100

    cenas

--- Projeto2/Trabalho_FSilva_JMarinho/Auto_Setup/SwitchSetup.tcl
+++ Projeto2/Trabalho_FSilva_JMarinho/Auto_Setup/SwitchSetup.tcl
@@ -34,14 +34,19 @@ Show bridges and ports:
 
 # criar bridges e ports
     # bridge120
+        # Tux123E1
+        # Tux124E1
 /interface bridge add name=bridge120
-/interface bridge port add bridge=bridge120 interface=ether16   # Tux123E1
-/interface bridge port add bridge=bridge120 interface=ether24   # Tux124E1
+/interface bridge port add bridge=bridge120 interface=ether16
+/interface bridge port add bridge=bridge120 interface=ether24
     # bridge121
+        # Tux122E1
+        # Tux124E2
+        # Tux124E2
 /interface bridge add name=bridge121
-/interface bridge port add bridge=bridge120 interface=ether8    # Tux122E1
-/interface bridge port add bridge=bridge120 interface=ether23   # Tux124E2
-/interface bridge port add bridge=bridge120 interface=ether21   # Router
+/interface bridge port add bridge=bridge121 interface=ether8
+/interface bridge port add bridge=bridge121 interface=ether23
+/interface bridge port add bridge=bridge121 interface=ether21
 
 
 teste
